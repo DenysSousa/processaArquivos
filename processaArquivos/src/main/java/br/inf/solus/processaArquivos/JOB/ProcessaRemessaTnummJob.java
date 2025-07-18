af5f9ba3e93abd90d88cb9143398cd0a67bcb3df
DenysSousa <66040446+DenysSousa@users.noreply.github.com>
2025-07-18 15:07:47 -0300

    🗑️Remoção do job para listener de processo

--- processaArquivos/src/main/java/br/inf/solus/processaArquivos/JOB/ProcessaRemessaTnummJob.java
+++ processaArquivos/src/main/java/br/inf/solus/processaArquivos/JOB/ProcessaRemessaTnummJob.java
@@ -1,12 +1,10 @@
 package br.inf.solus.processaArquivos.JOB;
 
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,13 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @EnableBatchProcessing
 public class ProcessaRemessaTnummJob {
 
-    @Autowired
-    JobExecutionListener jobFinalizadoListener;
-
     @Bean
     public Job jobProcessaRemessaTnumm(JobRepository jobRepository, Step stepRemessa, Step stepRegistroTnumm) {
         return new JobBuilder("jobProcessaRemessaTnumm", jobRepository)
-                .listener(jobFinalizadoListener)
                 .start(stepRemessa)
                 .next(stepRegistroTnumm)
                 .build();
